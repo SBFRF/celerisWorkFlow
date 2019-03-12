e6b7239648aaba4f9b80aeca2c61d19fe5824bcb
Spicer Bak <8375832+SBFRF@users.noreply.github.com>
2019-03-12 08:11:43 -0700

    updated work flow with a few enhancements and added documentation

--- WorkFlow_celeris.m
+++ WorkFlow_celeris.m
@@ -1,9 +1,27 @@
 %% Celeris master Work flow for Coastal Model Test Bed
 %    This script can operate in hindcast mode by inputing start and end 
-%    dates or with 
-
-simStart = '2019-01-24T13.00.00.Z'; % hindcast date in Z
-simEnd = '2019-01-24T14.00.00.Z'; % end date for batchruns 
+%    dates or in Nowcast mode modeling the most recent day
+%    
+%   INPUTS:
+%       simStart - time to start a batch of simulation(s).  If left blank (ie. []) 
+%           then will model the most recent day.  To model in hindcast input
+%           time in format of '2019-01-24T13.00.00.Z'.  All times are in
+%           UTC to match server time. 
+%       simEnd - time to end a batch of simulation(s).  If left blank (ie. []) 
+%           then will model the most recent day.  To model in hindcast input
+%           time in format of '2019-01-24T13.00.00.Z'.  All times are in
+%           UTC to match server time. 
+%       homeDir - this is where the celeris model exe and all
+%           supplimental files are located.  must also have the yaml_files
+%           directory to make netCDF files
+%       hourDT - this is the increment to run the model at (default is 1,
+%           for every hour)
+%
+%
+%
+%% set inputs 
+simStart = []; % hindcast date in Z
+simEnd =  [];  % end date for batchruns 
 % set home working directory 
 homeDir = "C:\Users\Waves.DESKTOP-DRJMBVD\Desktop\Celeris_dump\Celeris_Duck\Celeris_Duck";
 % wall clock time (in seconds)  should be 30, will save the last 17 minutes
@@ -15,9 +33,11 @@ hourDT = 1;
 if isempty(simStart);
     endSimdatetime = dateshift(datetime('now'), 'start', 'hour');
     startSimdatetime = endSimdatetime - hours(24);
+    hourDT = 1;
 else;
     endSimdatetime = datetime(simEnd, 'InputFormat','yyyy-MM-dd''T''HH.mm.ss''.Z');
     startSimdatetime = datetime(simStart, 'InputFormat','yyyy-MM-dd''T''HH.mm.ss''.Z');
+    hourDT = hourDT; 
 end
 % generate simulation list 
 simList = startSimdatetime:hours(hourDT):endSimdatetime;
